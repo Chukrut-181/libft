dfab193c0527c8e3a8c8e2b540249ad52ced3970
Chukrut-181 <161496032+Chukrut-181@users.noreply.github.com>
2024-11-05 11:18:31 +0100

    Added exhaustive comment.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,15 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igchurru <igchurru@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:53:42 by igchurru          #+#    #+#             */
-/*   Updated: 2024/04/22 15:54:20 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/11/05 11:11:35 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*	ft_strncmp:
+		Compares up to 'n' characters of two null-terminated strings
+		lexicographically.
+	Parameters:
+		s1 - The first string to compare.
+		s2 - The second string to compare.
+		n  - The maximum number of characters to compare.
+	Return:
+		0 if both strings are equal up to the first 'n' characters.
+		A positive integer if the first character that does not match
+		has a greater value in 's1' than in 's2'.
+		A negative integer if the first character that does not match
+		has a lesser value in 's1' than in 's2'.
+	Notes:
+		- Comparison is case-sensitive.
+		- The comparison stops when 'n' characters have been checked
+		or a null character is encountered in either string.
+		- The function converts both 's1' and 's2' to unsigned char pointers 
+		to ensure that characters are compared correctly as unsigned values.	*/
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned char	*ucs1;
@@ -34,10 +53,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 /* int	main(void)
 {
-	printf ("Diferencia: %d.\n", ft_strncmp ("123456789", "123456789", 9));
+	printf ("Diferencia: %d\n", ft_strncmp ("123456789", "123456789", 9));
 	return (0);
 } */
-
-/* ft_strncmp compara dos cadenas (s1 y s2) hasta un máximo de
-n caracteres. Devuelve 0 si las cadenas son iguales en esos caracteres,
-1 si s1 es mayor que s2, o -1 si s1 es menor que s2. */
