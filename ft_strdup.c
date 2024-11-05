da7c4946bfa333d1353003bfd5060cb5a9c077df
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2024-11-05 13:08:50 +0100

    Added exhaustive comment.

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,15 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igchurru <igchurru@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:41:03 by igchurru          #+#    #+#             */
-/*   Updated: 2024/04/30 11:14:42 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:07:38 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*	ft_strdup:
+		Allocates sufficient memory for a copy of the string 's1', 
+		does the copy, and returns a pointer to it.
+	Parameters:
+		s1 - The string to be duplicated.
+	Return:
+		A pointer to the duplicated string, or NULL if the allocation fails.
+		The duplicated string will include the null terminator.
+	Note:
+		The caller is responsible for freeing the memory allocated for 
+		the duplicated string.	*/
 char	*ft_strdup(const char *s1)
 {
 	char	*c;
@@ -23,6 +34,3 @@ char	*ft_strdup(const char *s1)
 		ft_strlcpy(c, s1, (ft_strlen(s1) + 1));
 	return (c);
 }
-
-/* 	if (s1 == NULL)
-		return (NULL); */
