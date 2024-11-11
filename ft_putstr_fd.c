6456c153292744c77447dcf7e8612424f35844e2
Chukrut-181 <161496032+Chukrut-181@users.noreply.github.com>
2024-11-11 16:50:37 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -12,13 +12,19 @@
 
 #include "libft.h"
 
+/*	ft_putstr_fd:
+		Writes the string 's' to the given file descriptor 'fd'. If the
+		string 's' is NULL or if the file descriptor is invalid (negative),
+		the function does nothing.
+	Parameters:
+		s  - The string to be written to the file descriptor.
+		fd - The file descriptor where the string will be written.
+	Return:
+		This function does not return a value. If 's' is NULL or
+		'fd' is invalid, it does nothing.	*/
 void	ft_putstr_fd(char *s, int fd)
 {
 	if (s == NULL || fd < 0)
 		return ;
 	write (fd, s, ft_strlen(s));
 }
-
-/* ft_putstr_fd escribe la cadena de caracteres s al archivo o dispositivo
-identificado por el descriptor de archivo fd. Verifica si el descriptor de
-archivo es válido y, si lo es, utiliza write para escribir la cadena. */
