e3cea21589b6699285b4084855db51c8700038ec
Chukrut-181 <161496032+Chukrut-181@users.noreply.github.com>
2024-11-11 17:01:15 +0100

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -12,6 +12,18 @@
 
 #include "libft.h"
 
+/*	ft_striteri:
+		Iterates over the string 's', passing each character to the function
+		'f'	along with its index. The function 'f' is applied to each character
+		in the string, modifying it in place if necessary.
+	Parameters:
+		s - The string to iterate over.
+		f - The function to apply to each character of the string, which takes 
+		an index and a pointer to the character.
+	Return:
+		This function does not return a value.
+		If either 's' or 'f' is NULL, the function returns without
+		doing anything.	*/
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
