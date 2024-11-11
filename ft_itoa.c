fbc965cac772e96edc57f7deccfff14bf0de7b62
Chukrut-181 <161496032+Chukrut-181@users.noreply.github.com>
2024-11-11 16:30:29 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,6 +12,14 @@
 
 #include "libft.h"
 
+/*	ft_int_len:
+		Calculates the length of the integer 'n' when represented as a string.
+		Takes into account the sign of the integer (for negative numbers).
+	Parameters:
+		n - The integer whose length is to be determined.
+	Return:
+		Returns the number of digits required to represent 'n' as a string.
+		Includes an extra digit for the negative sign if 'n' is negative.	*/
 static size_t	ft_int_len(int n)
 {
 	size_t	len;
@@ -27,6 +35,15 @@ static size_t	ft_int_len(int n)
 	return (len);
 }
 
+/*	ft_specialcase:
+		Handles the special cases for integer to string conversion.
+		Specifically, handles the edge case for the minimum integer value 
+		(-2147483648) and zero (0).
+	Parameters:
+		n - The integer to check.
+	Return:
+		Returns a string representation of the special case
+		(either "-2147483648" for the minimum integer or "0" for zero).	*/
 static char	*ft_specialcase(int n)
 {
 	if (n == -2147483648)
@@ -35,6 +52,18 @@ static char	*ft_specialcase(int n)
 		return (ft_strdup("0"));
 }
 
+/*	ft_itoa:
+		Converts an integer 'n' to its string representation. Handles negative
+		numbers, and special cases for zero and the minimum integer.
+	Parameters:
+		n - The integer to be converted.
+	Return:
+		Returns a string representing the integer 'n'.
+		If memory allocation fails, returns NULL.
+	Note:
+		This function handles the edge case of the minimum integer (-2147483648) 
+		since it cannot be represented as a positive value due to the limits of 
+		signed integers.	*/
 char	*ft_itoa(int n)
 {
 	char	*str;
@@ -72,8 +101,3 @@ char	*ft_itoa(int n)
 	free(result);
 	return (0);
 } */
-
-/* ft_itoa convierte un número entero en una cadena de caracteres,
-manejando casos especiales y números negativos, y asignando memoria
-dinámica para la cadena resultante. Devuelve un puntero a la cadena
-resultante. */
