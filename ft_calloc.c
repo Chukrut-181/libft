5c31c7984dc9beca7f5713038c5f4dd8cd0287bb
Chukrut-181 <161496032+Chukrut-181@users.noreply.github.com>
2024-11-11 16:12:56 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,6 +12,19 @@
 
 #include "libft.h"
 
+/*	ft_calloc:
+		Allocates memory for an array of 'count' elements, each of 'size' bytes,
+		and initializes all bytes to zero.
+	Parameters:
+		count - The number of elements to allocate memory for.
+		size  - The size in bytes of each element.
+	Return:
+		A pointer to the allocated memory, or NULL if the allocation fails.
+		Each byte of the allocated memory is set to zero.
+	Note:
+		This function mimics the behavior of the standard calloc function.
+		If either 'count' or 'size' is 0, the function still allocates memory,
+		but the size of the allocation will be 0.	*/
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t	s;
