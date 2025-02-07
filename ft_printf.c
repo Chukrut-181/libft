baa7cf4f45217f3ac7bebc6d6701b782d4101a33
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-07 15:30:14 +0100

    Updated ft_printf.

--- ft_printf.c
+++ ft_printf.c
@@ -6,64 +6,44 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 15:10:02 by igchurru          #+#    #+#             */
-/*   Updated: 2025/01/22 15:10:23 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:27:25 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*	ft_checkpointer - Prints a pointer address in hexadecimal format.
-@adr: Memory address to print.
-@q: Counter for printed characters.
-Returns: Updated counter after printing. */
-static int	ft_checkpointer(size_t adr, int q)
+static void	ft_checkpointer(size_t adr, int *q)
 {
 	if (!adr)
-		q = ft_printstr("(nil)", q);
+		ft_printstr("(nil)", q);
 	else
 	{
-		q = ft_printstr("0x", q);
-		q = ft_printhex(adr, q, 'x');
+		ft_printstr("0x", q);
+		ft_printhex(adr, q, 'x');
 	}
-	return (q);
 }
 
-/*	ft_discriminate - Processes format specifiers and calls relevant functions.
-@d: Format specifier character.
-@list: Variable argument list.
-@q: Counter for printed characters.
-Returns: Updated counter after printing. */
-static int	ft_discriminate(char d, va_list list, int q)
+static void	ft_discriminate(char d, va_list list, int *q)
 {
 	if (d == 'c')
-		q = (ft_printchar(va_arg(list, int), q));
+		*q += ft_printchar(va_arg(list, int));
 	else if (d == 's')
-		q = (ft_printstr(va_arg(list, char *), q));
+		ft_printstr(va_arg(list, char *), q);
 	else if (d == 'p')
-		q = ft_checkpointer(va_arg(list, size_t), q);
+		ft_checkpointer(va_arg(list, size_t), q);
 	else if (d == 'i' || d == 'd')
-		q = ft_printnbr(va_arg(list, int), q);
+		ft_printnbr(va_arg(list, int), q);
 	else if (d == 'u')
-		q = ft_printunsnbr(va_arg(list, unsigned int), q);
+		ft_printunsnbr(va_arg(list, unsigned int), q);
 	else if (d == 'x')
-		q = (ft_printhex(va_arg(list, unsigned int), q, 'x'));
+		ft_printhex(va_arg(list, unsigned int), q, 'x');
 	else if (d == 'X')
-		q = (ft_printhex(va_arg(list, unsigned int), q, 'X'));
+		ft_printhex(va_arg(list, unsigned int), q, 'X');
 	else if (d == '%')
-		q = (ft_printchar('%', q));
-	return (q);
+		*q += ft_printchar('%');
+	else
+		return ;
 }
 
-/*	ft_printf - Mimics the C standard library's printf function.
-@str: Format string containing text and format specifiers.
-Returns: Total number of characters printed.
-
-Initialize the variable argument list.
-Check for format specifier.
-Process specifier.
-Print regular characters.
-Clean up variable argument list.
-Return total characters printed. */
 int	ft_printf(char const *str, ...)
 {
 	va_list	list;
@@ -78,12 +58,10 @@ int	ft_printf(char const *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
-			q = ft_discriminate(str[i], list, q);
+			ft_discriminate(str[i], list, &q);
 		}
 		else
-		{
-			q = ft_printchar(str[i], q);
-		}
+			q += ft_printchar(str[i]);
 		i++;
 	}
 	va_end(list);
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,31 +6,18 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 15:14:23 by igchurru          #+#    #+#             */
-/*   Updated: 2025/01/22 15:14:34 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:28:10 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*	ft_printchar - Writes a character to standard output.
-@c: Character to print.
-@q: Counter for printed characters.
-Returns: Updated counter after printing. */
-int	ft_printchar(char c, int q)
+int	ft_printchar(char c)
 {
-	write (1, &c, 1);
-	q++;
-	return (q);
+	return (write (1, &c, 1));
 }
 
-/*	ft_printstr - Writes a string to standard output.
-@str: String to print.
-@q: Counter for printed characters.
-Returns: Updated counter after printing. 
-
-Handle NULL string by printing "(null)".
-Print each character of the string. */
-int	ft_printstr(char *str, int q)
+void	ft_printstr(char *str, int *q)
 {
 	int	i;
 
@@ -39,72 +26,41 @@ int	ft_printstr(char *str, int q)
 	i = 0;
 	while (str[i])
 	{
-		q = ft_printchar(str[i], q);
+		*q += ft_printchar(str[i]);
 		i++;
 	}
-	return (q);
 }
 
-/*	ft_printhex - Recursively prints a number in hexadecimal format.
-@n: Unsigned number to print in hexadecimal.
-@q: Counter for printed characters.
-@h: Hexadecimal format ('x' for lowercase, 'X' for uppercase).
-Returns: Updated counter after printing.
-
-Recursive division by 16 to get all digits. */
-int	ft_printhex(unsigned long n, int q, char h)
+void	ft_printhex(unsigned long n, int *q, char h)
 {
 	if (15 < n)
-		q = ft_printhex(n / 16, q, h);
+		ft_printhex(n / 16, q, h);
 	if (h == 'x' || h == 'p')
-		q = ft_printchar("0123456789abcdef"[n % 16], q);
-	if (h == 'X')
-		q = ft_printchar("0123456789ABCDEF"[n % 16], q);
-	return (q);
+		*q += ft_printchar("0123456789abcdef"[n % 16]);
+	else if (h == 'X')
+		*q += ft_printchar("0123456789ABCDEF"[n % 16]);
 }
 
-/*	ft_printnbr - Recursively prints a signed integer.
-@n: Integer to print.
-@q: Counter for printed characters.
-Returns: Updated counter after printing.
-
-Handle INT_MIN explicitly.
-Print negative sign for negative numbers.
-Recursive division by 10 to get all digits.
-Print each digit */
-int	ft_printnbr(int n, int q)
+void	ft_printnbr(int n, int *q)
 {
 	if (n == -2147483648)
 	{
-		q = ft_printstr("-2147483648", q);
-		return (q);
+		ft_printstr("-2147483648", q);
+		return ;
 	}
 	if (n < 0)
 	{
-		q = ft_printchar('-', q);
+		*q += ft_printchar('-');
 		n = -n;
 	}
 	if (9 < n)
-	{
-		q = ft_printnbr(n / 10, q);
-	}
-	q = ft_printchar((n % 10 + '0'), q);
-	return (q);
+		ft_printnbr(n / 10, q);
+	*q += ft_printchar((n % 10 + '0'));
 }
 
-/*	ft_printunsnbr - Recursively prints an unsigned integer.
-@n: Unsigned integer to print.
-@q: Counter for printed characters.
-Returns: Updated counter after printing.
-
-Recursive division by 10 to get all digits.
-Print each digit. */
-int	ft_printunsnbr(unsigned int n, int q)
+void	ft_printunsnbr(unsigned int n, int *q)
 {
 	if (9 < n)
-	{
-		q = ft_printunsnbr(n / 10, q);
-	}
-	q = ft_printchar((n % 10 + '0'), q);
-	return (q);
+		ft_printunsnbr(n / 10, q);
+	*q += ft_printchar((n % 10 + '0'));
 }
